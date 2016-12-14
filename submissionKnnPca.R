cd6ff15d437b5e18ecbd1f22e87cb34b74fe0eb0
Michael Kinsey <michael.kinsey@umontana.edu>
2016-12-14 00:42:47 -0700

    Add final submission script. scored 97.3%

---
+++ submissionKnnPca.R
@@ -0,0 +1,23 @@
+# MKinsey
+# Digit Classification using KNN
+
+library(class)
+
+train <- read.csv("train.csv")
+test  <- read.csv("test.csv")
+
+trainSet = train[, -1]
+trainLabels = train[, 1]
+
+# KNN
+# PCA using SVD
+dPca = prcomp(trainSet)
+trainPca = as.matrix(trainSet) %*% dPca$rotation[ , 1:150]  # columns are eigenvectors, use first 150 principal components
+testPca = as.matrix(test) %*% dPca$rotation[ , 1:150]
+fitPca = knn(trainPca, testPca, trainLabels)
+
+# format output
+finalprediction<- cbind(as.data.frame(1:length(fitPca)),fitPca)
+colnames(finalprediction) <- c("ImageId","Label")
+# write to file
+write.csv(finalprediction,file="predictions.csv",row.names=FALSE)
